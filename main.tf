04d2742b9812fc50b11565b51c8924dbdab1fd41
sasidhar sunny <63310620+sasidharsunny@users.noreply.github.com>
2024-08-14 15:18:38 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.39.1"
+    }
+  }
+}
+
+provider "google" {
+  project = "silent-robot-427709-j7"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+
+}
+
+
+
+resource "google_storage_bucket" "auto-expire" {
+  name                     = "terraform-buucket1"
+  location                 = "us-central1"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+
