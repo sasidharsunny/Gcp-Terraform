aa1e976983ae59aa0e9c191ca8ecb156db05cbba
sasidhar sunny <63310620+sasidharsunny@users.noreply.github.com>
2024-08-14 15:03:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,2 @@
 
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.39.1"
-    }
-  }
-}
-
-provider "google" {
-  project = "silent-robot-427709-j7"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-
-}
-
-
-
-resource "google_storage_bucket" "static-site" {
-  name                     = "terraformnew0909090"
-  location                 = "us-east1"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-}
-
-
 
