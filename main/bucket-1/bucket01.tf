a5f5a02c4a6e237238fb9553063414a0591a77e2
sasidhar sunny <63310620+sasidharsunny@users.noreply.github.com>
2024-08-14 15:02:34 +0530

    Update bucket01.tf

--- main/bucket-1/bucket01.tf
+++ main/bucket-1/bucket01.tf
@@ -1,1 +1,6 @@
-
+resource "google_storage_bucket" "static-site-1" {
+  name                     = "terraformnew080808"
+  location                 = "us-east1"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
